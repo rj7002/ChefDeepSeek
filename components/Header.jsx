401028a409622506620be394f83730d281b46c5f
Ryan Joseph <160825414+rj7002@users.noreply.github.com>
2025-08-11 15:21:41 -0700

    Add files via upload

---
+++ components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+        <>
+        <header>
+            <img src="https://media.craiyon.com/2025-06-06/Wg8HV40IRRiIksCx-gtxdw.webp" alt="robot-logo"/>
+            <h1>Chef DeepSeek</h1>
+        </header>
+        </>
+    )
+}
